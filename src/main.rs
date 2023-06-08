e6ffe585518cfa7cf75dad3a3561cef78c81c252
stoykotolevsenteca <stoyko.tolev@sentecacommerce.com>
2023-06-08 21:54:36 +0300

    feat: move fetching to separate function

--- src/main.rs
+++ src/main.rs
@@ -10,8 +10,7 @@ struct WordleData {
     editor: String,
 }
 
-// let resp = reqwest::blocking::get("https://www.nytimes.com/svc/wordle/v2/2023-06-05.json")?
-fn main() -> Result<(), Box<dyn std::error::Error>> {
+fn get_data() -> Result<Vec<char>, Box<dyn std::error::Error>> {
     let current_date = Local::now().date_naive();
     let wordle_url = format!(
         "https://www.nytimes.com/svc/wordle/v2/{}.json",
@@ -20,7 +19,17 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
     let resp = reqwest::blocking::get(wordle_url)?.json::<WordleData>()?;
 
-    let word_of_the_day: Vec<char> = resp.solution.chars().collect();
+    Ok(resp.solution.chars().collect::<Vec<char>>())
+}
 
-    Ok(())
+// let resp = reqwest::blocking::get("https://www.nytimes.com/svc/wordle/v2/2023-06-05.json")?
+fn main() {
+    let mut tries: i8 = 0;
+
+    let mut word_of_the_day: Vec<char>;
+
+    match get_data() {
+        Ok(value) => println!("Values: {:?}", value),
+        Err(err) => println!("Error: {}", err),
+    }
 }
