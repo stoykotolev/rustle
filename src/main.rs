73cb7308ea8ceaa392592451894c2b79c8f5832c
stoykotolevsenteca <stoyko.tolev@sentecacommerce.com>
2023-06-11 09:52:58 +0300

    feat: move user input inside the loop. add correct colors

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,5 @@
+use std::io;
+
 use chrono::Local;
 use serde::{Deserialize, Serialize};
 
@@ -24,7 +26,7 @@ fn get_data() -> Result<Vec<char>, Box<dyn std::error::Error>> {
 
 // let resp = reqwest::blocking::get("https://www.nytimes.com/svc/wordle/v2/2023-06-05.json")?
 fn main() {
-    let mut tries: i8 = 0;
+    let mut tries: i8 = 1;
 
     let word_of_the_day: Vec<char> = match get_data() {
         Ok(value) => value,
@@ -38,4 +40,39 @@ fn main() {
         "This is the word for the day: {:?}",
         word_of_the_day.iter().collect::<String>()
     );
+
+    while tries <= 5 {
+        let mut input_string = String::new();
+        let mut user_guess: [String; 5] = [
+            String::from("_"),
+            String::from("_"),
+            String::from("_"),
+            String::from("_"),
+            String::from("_"),
+        ];
+
+        io::stdin().read_line(&mut input_string).unwrap();
+
+        if input_string.trim().len() > 6 {
+            println!("Please enter a 5 letter word");
+            continue;
+        }
+
+        if input_string.trim().len() < 5 {
+            println!("Please enter a 5 letter word");
+            continue;
+        }
+
+        for (index, char) in input_string.chars().enumerate() {
+            if word_of_the_day.contains(&char) {
+                if word_of_the_day[index] == char {
+                    user_guess[index] = format!("\x1b[1;32m{}\x1b[0m", char);
+                } else {
+                    user_guess[index] = format!("\x1b[0;33m{}\x1b[0m", char)
+                }
+            }
+        }
+        tries += 1;
+    }
+    println!("almost");
 }
